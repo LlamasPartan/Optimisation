5958ee5746777bff9948130be189508f3d2a2896
Smaxy63 <63102199+Smaxy63@users.noreply.github.com>
2020-08-30 15:58:26 +0200

    Update phases.m

--- Simplexe/Méthode des phases/phases.m
+++ Simplexe/Méthode des phases/phases.m
@@ -1,5 +1,5 @@
 %Initialisation
-tab=[
+tab1=[
 142,241,0,0,0,0;
 0.23,0.7,1,0,0,20.21;
 0.015,0.015,0,1,0,0.705;
@@ -11,6 +11,39 @@ tab2=[
 5,-4,0,1,0,1;
 -3,1,0,0,1,-1]
 
+function indiceVarEntrante=variableEntrante(tab)
+    indiceVarEntrante=1
+    for i=2:size(tab)(2)-1
+        if tab(1,i)>tab(1,indiceVarEntrante)
+            indiceVarEntrante=i
+            disp("a")
+        endif
+    endfor
+endfunction
+
+function indiceVarSortante=variableSortante(tab,indiceVarEntrante)
+    indiceVarSortante=1
+    rapportMin=Inf
+    for i=2:size(tab)(1)
+        if tab(i,indiceVarEntrante)>0
+            rapport=tab(i,size(tab)(2))/tab(i,indiceVarEntrante)
+            if rapport<rapportMin && rapport>=0
+                indiceVarSortante=i
+                rapportMin=rapport
+            endif
+        endif
+    endfor
+endfunction
+
+function tab=normalisation(tab,indiceVarEntrante,indiceVarSortante)
+    tab(indiceVarSortante,:)=1/tab(indiceVarSortante,indiceVarEntrante)*tab(indiceVarSortante,:)
+    for i=1:size(tab)(1)
+        if i!=indiceVarSortante
+            tab(i,:)-=tab(i,indiceVarEntrante)*tab(indiceVarSortante,:)
+        endif
+    endfor
+endfunction
+    
 function [tab,x]=simplexeStandard(tab)
     %On crée un vecteur dont l'i-ème élément est l'indice de la variable dont la
     %valeur est à la fin de la i+1-ème ligne du tableau
@@ -24,36 +57,15 @@ function [tab,x]=simplexeStandard(tab)
             endif
         endfor
         %Recherche de la variable entrante
-        indiceVarEntrante=1
-        for i=2:size(tab)(2)-1
-            if tab(1,i)>tab(1,indiceVarEntrante)
-                indiceVarEntrante=i
-            endif
-        endfor
+        indiceVarEntrante=variableEntrante(tab)
         %Recherche de la variable sortante
-        indiceVarSortante=1
-        rapportMin=Inf
-        for i=2:size(tab)(1)
-            if tab(i,indiceVarEntrante)>0
-                rapport=tab(i,size(tab)(2))/tab(i,indiceVarEntrante)
-                if rapport<rapportMin && rapport>=0
-                    indiceVarSortante=i
-                    rapportMin=rapport
-                endif
-            endif
-        endfor
+        indiceVarSortante=variableSortante(tab,indiceVarEntrante)
         if indiceVarSortante==1
             disp("Pas de variable sortante")
             return
         endif
-        %Coefficient principal à 1
-        tab(indiceVarSortante,:)=1/tab(indiceVarSortante,indiceVarEntrante)*tab(indiceVarSortante,:)
-        %Coefficients secondaires à 0
-        for i=1:size(tab)(1)
-            if i!=indiceVarSortante
-                tab(i,:)-=tab(i,indiceVarEntrante)*tab(indiceVarSortante,:)
-            endif
-        endfor
+        %Coefficient principal à 1 et coefficients secondaires à 0
+        tab=normalisation(tab,indiceVarEntrante,indiceVarSortante)
         %Stockage de la position des différentes variables
         x(indiceVarSortante-1)=indiceVarEntrante
     end
@@ -68,6 +80,17 @@ function [tabDelta,x]=simplexePhases(tab,x)
     tabDroite=tabDelta(:,end)
     tabDelta=[tabGauche,tabMilieu,tabDroite]
     %Première phase
+    [tabDelta,x]=phase1(tabDelta,x)
+    %On regarde si on peut passer à la seconde phase
+    if tabDelta(1,end)>0
+        disp("Le problème n'a pas d'optimum")
+        return
+    endif
+    %Seconde phase
+    [tabDelta,x]=phase2(tabDelta,x,tab)
+endfunction
+
+function [tabDelta,x]=phase1(tabDelta,x)
     %Choix de la variable entrante (delta)
     indiceVarEntrante=size(tabDelta)(2)-1
     %Recherche de la variable sortante (la plus petite)
@@ -77,35 +100,25 @@ function [tabDelta,x]=simplexePhases(tab,x)
             indiceVarSortante=i
         endif
     endfor
-    %Coefficient principal à 1
-    tabDelta(indiceVarSortante,:)=1/tabDelta(indiceVarSortante,indiceVarEntrante)*tabDelta(indiceVarSortante,:)
-    %Coefficients secondaires à 0
-    for i=1:size(tabDelta)(1)
-        if i!=indiceVarSortante
-            tabDelta(i,:)-=tabDelta(i,indiceVarEntrante)*tabDelta(indiceVarSortante,:)
-        endif
-    endfor
+    %Coefficient principal à 1 et coefficients secondaires à 0
+    tabDelta=normalisation(tabDelta,indiceVarEntrante,indiceVarSortante)
     %Stockage de la position des différentes variables
     x(indiceVarSortante-1)=indiceVarEntrante
     %Tant que les coefficients de Lf dont positifs
     while sum(tabDelta(1,1:end-1)>0)>0
         %Recherche de la variable entrante
-        indiceVarEntrante=1
-        for i=2:size(tabDelta)(2)-1
-            if tabDelta(1,i)>tabDelta(1,indiceVarEntrante)
-                indiceVarEntrante=i
-            endif
-        endfor
-        %Recherche de la variable sortante
+        indiceVarEntrante=variableEntrante(tabDelta)
+        %Recherche de la variable sortante en privilégiant delta
         indiceVarSortante=1
         rapportMin=Inf
         for i=2:size(tabDelta)(1)
             if tabDelta(i,indiceVarEntrante)>0
                 rapport=tabDelta(i,size(tabDelta)(2))/tabDelta(i,indiceVarEntrante)
-                if rapport==rapportMin && x(i)==size(tabDelta)(2)-1
+                if x(i-1)==size(tabDelta)(2)-1 && rapport==rapportMin
                     indiceVarSortante=i
                     rapportMin=rapport
-                elseif rapport<rapportMin && rapport>=0
+                endif
+                if rapport<rapportMin && rapport>=0
                     indiceVarSortante=i
                     rapportMin=rapport
                 endif
@@ -115,23 +128,14 @@ function [tabDelta,x]=simplexePhases(tab,x)
             disp("Pas de variable sortante")
             return
         endif
-        %Coefficient principal à 1
-        tabDelta(indiceVarSortante,:)=1/tabDelta(indiceVarSortante,indiceVarEntrante)*tabDelta(indiceVarSortante,:)
-        %Coefficients secondaires à 0
-        for i=1:size(tabDelta)(1)
-            if i!=indiceVarSortante
-                tabDelta(i,:)-=tabDelta(i,indiceVarEntrante)*tabDelta(indiceVarSortante,:)
-            endif
-        endfor
+        %Coefficient principal à 1 et coefficients secondaires à 0
+        tabDelta=normalisation(tabDelta,indiceVarEntrante,indiceVarSortante)
         %Stockage de la position des différentes variables
         x(indiceVarSortante-1)=indiceVarEntrante
     end
-    %On regarde si on peut passer à la seconde phase
-    if tabDelta(1,end)>0
-        disp("Le problème n'a pas d'optimum")
-        return
-    endif
-    %Seconde phase
+endfunction
+
+function [tabDelta,x]=phase2(tabDelta,x,tab)
     %Suppression de la colonne delta
     tabGauche=tabDelta(:,1:end-2)
     tabDroite=tabDelta(:,end)
@@ -144,7 +148,7 @@ function [tabDelta,x]=simplexePhases(tab,x)
     endfor
 endfunction
 
-[tabRes,xRes]=simplexeStandard(tab)
+[tabRes,xRes]=simplexeStandard(tab2)
 disp(tabRes)
 for i=1:size(xRes)(2)
     if xRes(i)<size(tabRes)(2)-size(tabRes)(1)+1
