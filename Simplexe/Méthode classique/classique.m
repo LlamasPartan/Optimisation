b5896e72986f40890c243007a1c44bd15afab235
Smaxy63 <63102199+Smaxy63@users.noreply.github.com>
2020-08-30 14:42:02 +0200

    Update classique.m

--- Simplexe/Méthode classique/classique.m
+++ Simplexe/Méthode classique/classique.m
@@ -1,10 +1,43 @@
 %Initialisation
-tab=[
+tab1=[
 142,241,0,0,0,0;
 0.23,0.7,1,0,0,20.21;
 0.015,0.015,0,1,0,0.705;
 1.8,1.1,0,0,1,75]
 
+function indiceVarEntrante=variableEntrante(tab)
+    indiceVarEntrante=1
+    for i=2:size(tab)(2)-1
+        if tab(1,i)>tab(1,indiceVarEntrante)
+            indiceVarEntrante=i
+            disp("a")
+        endif
+    endfor
+endfunction
+
+function indiceVarSortante=variableSortante(tab,indiceVarEntrante)
+    indiceVarSortante=1
+    rapportMin=Inf
+    for i=2:size(tab)(1)
+        if tab(i,indiceVarEntrante)>0
+            rapport=tab(i,size(tab)(2))/tab(i,indiceVarEntrante)
+            if rapport<rapportMin && rapport>=0
+                indiceVarSortante=i
+                rapportMin=rapport
+            endif
+        endif
+    endfor
+endfunction
+
+function tab=normalisation(tab,indiceVarEntrante,indiceVarSortante)
+    tab(indiceVarSortante,:)=1/tab(indiceVarSortante,indiceVarEntrante)*tab(indiceVarSortante,:)
+    for i=1:size(tab)(1)
+        if i!=indiceVarSortante
+            tab(i,:)-=tab(i,indiceVarEntrante)*tab(indiceVarSortante,:)
+        endif
+    endfor
+endfunction
+    
 function [tab,x]=simplexeStandard(tab)
     %On crée un vecteur dont l'i-ème élément est l'indice de la variable dont la
     %valeur est à la fin de la i+1-ème ligne du tableau
@@ -12,42 +45,21 @@ function [tab,x]=simplexeStandard(tab)
     %Tant que les coefficients de Lf dont positifs
     while sum(tab(1,1:end-1)>0)>0
         %Recherche de la variable entrante
-        indiceVarEntrante=1
-        for i=2:size(tab)(2)-1
-            if tab(1,i)>tab(1,indiceVarEntrante)
-                indiceVarEntrante=i
-            endif
-        endfor
+        indiceVarEntrante=variableEntrante(tab)
         %Recherche de la variable sortante
-        indiceVarSortante=1
-        rapportMin=Inf
-        for i=2:size(tab)(1)
-            if tab(i,indiceVarEntrante)>0
-                rapport=tab(i,size(tab)(2))/tab(i,indiceVarEntrante)
-                if rapport<rapportMin && rapport>=0
-                    indiceVarSortante=i
-                    rapportMin=rapport
-                endif
-            endif
-        endfor
+        indiceVarSortante=variableSortante(tab,indiceVarEntrante)
         if indiceVarSortante==1
             disp("Pas de variable sortante")
             return
         endif
-        %Coefficient principal à 1
-        tab(indiceVarSortante,:)=1/tab(indiceVarSortante,indiceVarEntrante)*tab(indiceVarSortante,:)
-        %Coefficients secondaires à 0
-        for i=1:size(tab)(1)
-            if i!=indiceVarSortante
-                tab(i,:)-=tab(i,indiceVarEntrante)*tab(indiceVarSortante,:)
-            endif
-        endfor
+        %Coefficient principal à 1 et coefficients secondaires à 0
+        tab=normalisation(tab,indiceVarEntrante,indiceVarSortante)
         %Stockage de la position des différentes variables
         x(indiceVarSortante-1)=indiceVarEntrante
     end
 endfunction
 
-[tabRes,xRes]=simplexeStandard(tab)
+[tabRes,xRes]=simplexeStandard(tab1)
 disp(tabRes)
 for i=1:size(xRes)(2)
     if xRes(i)<size(tabRes)(2)-size(tabRes)(1)+1
@@ -57,3 +69,4 @@ for i=1:size(xRes)(2)
     endif
 endfor
 fprintf("Optimum de %d\n",-tabRes(1,end))
+
