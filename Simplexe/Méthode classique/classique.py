fb2ff245f75edbe9995c39fb35e90b1d8191e707
Smaxy63 <63102199+Smaxy63@users.noreply.github.com>
2020-08-30 14:37:39 +0200

    Update classique.py

--- Simplexe/Méthode classique/classique.py
+++ Simplexe/Méthode classique/classique.py
@@ -1,12 +1,36 @@
 import numpy as np
+np.set_printoptions(linewidth=99999) #Pour afficher les longues matrices dans la console sans aller à la ligne...
 
 #Initialisation
-tab=np.array([
-[10.,-57,-9,-24,100,0,0,0,0,0],
-[0,0,0,0,1,1,0,0,0,1],
-[0.5,-5.5,-2.5,9,1,0,1,0,0,1],
-[0.5,-1.5,-0.5,1,1,0,0,1,0,1],
-[1,0,0,0,1,0,0,0,0,2]])
+tab1=np.array([
+[142,241,0,0,0,0],
+[0.23,0.7,1,0,0,20.21],
+[0.015,0.015,0,1,0,0.705],
+[1.8,1.1,0,0,1,75]])
+
+def variableEntrante(tab):
+    indiceVarEntrante=0
+    for i in range(1,tab.shape[1]-1):
+        if tab[0,i]>tab[0,indiceVarEntrante]:
+            indiceVarEntrante=i
+    return(indiceVarEntrante)
+
+def variableSortante(tab,indiceVarEntrante):
+    indiceVarSortante=0
+    rapportMin=np.inf
+    for i in range(1,tab.shape[0]):
+        if tab[i,indiceVarEntrante]>0:
+            rapport=tab[i,tab.shape[1]-1]/tab[i,indiceVarEntrante]
+            if rapport<rapportMin and rapport>=0:
+                indiceVarSortante=i
+                rapportMin=rapport
+    return(indiceVarSortante)
+
+def normalisation(tab,indiceVarEntrante,indiceVarSortante):
+    tab[indiceVarSortante,:]=1/tab[indiceVarSortante,indiceVarEntrante]*tab[indiceVarSortante,:]
+    for i in range(tab.shape[0]):
+        if i!=indiceVarSortante:
+            tab[i,:]-=tab[i,indiceVarEntrante]*tab[indiceVarSortante,:]
 
 def simplexeStandard(tab):
     #On crée un vecteur dont l'i-ème élément est l'indice de la variable dont la
@@ -15,37 +39,23 @@ def simplexeStandard(tab):
     #Tant que les coefficients de Lf sont positifs
     while (tab[0,:-1]>np.zeros((1,tab.shape[1]-1))).any():
         #Recherche de la variable entrante
-        indiceVarEntrante=0
-        for i in range(1,tab.shape[1]-1):
-            if tab[0,i]>tab[0,indiceVarEntrante]:
-                indiceVarEntrante=i
+        indiceVarEntrante=variableEntrante(tab)
         #Recherche de la variable sortante
-        indiceVarSortante=0
-        rapportMin=np.inf
-        for i in range(1,tab.shape[0]):
-            if tab[i,indiceVarEntrante]>0:
-                rapport=tab[i,tab.shape[1]-1]/tab[i,indiceVarEntrante]
-                if rapport<rapportMin and rapport>=0:
-                    indiceVarSortante=i
-                    rapportMin=rapport
+        indiceVarSortante=variableSortante(tab,indiceVarEntrante)
         if indiceVarSortante==0:
             print("Pas de variable sortante")
             exit()
-        #Coefficient principal à 1
-        tab[indiceVarSortante,:]=1/tab[indiceVarSortante,indiceVarEntrante]*tab[indiceVarSortante,:]
-        #Coefficients secondaires à 0
-        for i in range(tab.shape[0]):
-            if i!=indiceVarSortante:
-                tab[i,:]-=tab[i,indiceVarEntrante]*tab[indiceVarSortante,:]
+        #Coefficient principal à 1 et coefficients secondaires à 0
+        normalisation(tab,indiceVarEntrante,indiceVarSortante)
         #Stockage de la position des différentes variables
         x[indiceVarSortante-1]=indiceVarEntrante
     return(tab,x)
 
-tab,x=simplexeStandard(tab)
+tab,x=simplexeStandard(tab1)
 print(tab)
 for i in range(x.size):
     if x[i]<tab.shape[1]-tab.shape[0]:
-        print("x",x[i]+1,"=",tab[i+1,-1])
+        print("x",x[i]+1,"=",np.around(tab[i+1,-1],3))
     else:
-        print("y",x[i]+1-tab.shape[1]+tab.shape[0],"=",tab[i+1,-1])
-print("Optimum de",-tab[0,-1])
+        print("y",x[i]+1-tab.shape[1]+tab.shape[0],"=",np.around(tab[i+1,-1],3))
+print("Optimum de",-np.around(tab[0,-1],3))
